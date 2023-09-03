83c954d263220acf9a268103c7910f627c35cd20
zaid alhakeem <zaidalhakeem86@gmail.com>
2023-09-03 07:45:28 -0500

    Adding week2homework

---
+++ week2homework.js
@@ -0,0 +1,15 @@
+let pizzaPlace ="Zaid's pizza";
+let numberOfToppings = 6;
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+console.log(`Welcome to ${pizzaPlace} and i have ${numberOfToppings} Toppings`);
+if ( numberOfToppings<10){
+  console.log("Quality, not quantity.")
+}
+else {
+  console.log("A whole lot of pizza.")
+}
+
+for (let i = 1; i <= 6; i++) {
+  console.log(i);
+}
