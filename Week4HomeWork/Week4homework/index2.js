fabd04777150f6c6f952a51503be502db23cc3b3
zaid alhakeem <zaidalhakeem86@gmail.com>
2023-09-18 17:54:31 -0500

    Week4HomeWork

---
+++ Week4HomeWork/Week4homework/index2.js
@@ -0,0 +1,6 @@
+document.getElementsByClassName("item")[0].style.color = "purple";
+document.getElementsByClassName("item")[1].style.color = "red";
+document.getElementsByClassName("item")[2].style.color = "green";
+document.getElementById("heading1").innerHTML = "it is my birthday! welcome";
+document.querySelector("a").style.color = "blue";
+document.getElementById("aelement").style.color = "red";
